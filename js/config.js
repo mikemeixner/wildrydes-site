ab70edaa5440a44b57dfc3dc30022a13365a41ad
Mike Meixner <73126172+mikemeixner@users.noreply.github.com>
2026-01-18 11:06:19 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_BGyEKgcGR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '9r58cocu1gi3h6lnf4g0ske30', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_qCxoVCHat', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1buj5q5itp39akneflpa31bovn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
